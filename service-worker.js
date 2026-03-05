1eb2d99e48c55e378c4fd9cc20192c18d1667376
naciondelte <naciondelte@gmail.com>
2026-03-05 01:30:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
-const CACHE_NAME="gongfu-cache-v1";
-const urlsToCache=["index.html"];
+const CACHE_NAME = "gongfu-cache-v1";
 
-self.addEventListener("install",event=>{
+const urlsToCache = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>{
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch",event=>{
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response=>{
-      return response||fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
